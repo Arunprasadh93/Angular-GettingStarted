2045aacc18a9ec29029dfc091aecf1e53e9bacc0
Arun Prasadh <arunprasadh.sj@gmail.com>
2020-04-28 19:47:37 +0530

    added custom pipes to filter

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './products'
+
 @Component({
     selector : 'pm-products',
     templateUrl : './product-list.component.html',
@@ -10,7 +11,32 @@ export class ProductListComponent implements OnInit{
     imageWidth : number = 50;
     imageMargin : number = 2;
     showImage : boolean = false;
-    listFilter : string = 'cart';
+    
+
+    _listFilter : string;
+
+    get listFilter() : string {
+        return this._listFilter;
+    }
+
+    set listFilter(value : string) {
+      this._listFilter = value;
+      this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
+    }
+
+    constructor() {
+      this.filteredProducts = this.products;
+      this._listFilter = 'cart';
+    }
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+
+    filteredProducts : IProduct[];
+
     products : IProduct[] = [
         {
           "productId": 1,
